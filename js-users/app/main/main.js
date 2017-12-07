4c5bae9aff64e2d2a65f293142ce6ab356ddb7a2
damiano.fossa <d.fossa.91@gmail.com>
2017-12-07 18:22:50 +0100

    pagination improved with better style

--- js-users/app/main/main.js
+++ js-users/app/main/main.js
@@ -1,14 +1,30 @@
-angular.module('Js-Users-App').controller('UsersList', UsersList);
+angular.module('Js-Users-App').factory('PagerService', PagerService).controller('UsersList', UsersList);
 
-function UsersList($route, $scope, $filter, userDataFactory){
+function UsersList($route, $scope, $filter, userDataFactory, PagerService){
 	$scope.users = []
 	$scope.currentPage = 0;
     $scope.pageSize = 10;
 
-	// Get the list of users
- 	userDataFactory.userList().then(function(response) {
-    	$scope.users = response.data;
-  	});
+    $scope.setPage = setPage;
+    $scope.pager = {};
+
+    // Get the list of users
+    userDataFactory.userList().then(function(response) {
+        $scope.users = response.data;
+        $scope.setPage(1);
+    });
+
+    function setPage(page) {
+        if (page < 1 || page > $scope.pager.totalPages) {
+            return;
+        }
+
+        // get pager object from service
+        $scope.pager = PagerService.GetPager($scope.users.length, page);
+
+        // get current page of items
+        $scope.items = $scope.users.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+    }
 
     // Get the number of pages from the number of users
   	$scope.numberOfPages = function(){
@@ -25,8 +41,8 @@ function UsersList($route, $scope, $filter, userDataFactory){
         }
 
         var putData = {
-          id: id,
-          status: newStatus
+            id: id,
+            status: newStatus
         };
 
         userDataFactory.editUser(id, putData).then(function(response) {
@@ -36,6 +52,66 @@ function UsersList($route, $scope, $filter, userDataFactory){
         }).catch(function(error) {
             console.log(error);
         });
+    }
+}
+
+function PagerService() {
+    // service definition
+    var service = {};
+ 
+    service.GetPager = GetPager;
+ 
+    return service;
+ 
+    // service implementation
+    function GetPager(totalItems, currentPage, pageSize) {
+        // default to first page
+        currentPage = currentPage || 1;
+ 
+        // default page size is 10
+        pageSize = pageSize || 10;
+ 
+        // calculate total pages
+        var totalPages = Math.ceil(totalItems / pageSize);
+ 
+        var startPage, endPage;
+        if (totalPages <= 10) {
+            // less than 10 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 10 total pages so calculate start and end pages
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+ 
+        // calculate start and end item indexes
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+ 
+        // create an array of pages to ng-repeat in the pager control
+        var pages = _.range(startPage, endPage + 1);
+ 
+        // return object with all pager properties required by the view
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
     }
 }
 
