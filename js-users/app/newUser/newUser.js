aeb8e518c7a2e00a1d0753e686a31686e8568480
damiano.fossa <d.fossa.91@gmail.com>
2017-12-07 14:21:53 +0100

    Response message after user creation

--- js-users/app/newUser/newUser.js
+++ js-users/app/newUser/newUser.js
@@ -2,21 +2,27 @@ angular.module('Js-Users-App').controller('NewUserCtrl', NewUserCtrl);
 
 
 function NewUserCtrl($route, $scope, userDataFactory){
+	$scope.user_created = false;
 
 	$scope.submitNewUser = function () {
 
-	    var postData = {
-            first_name: $scope.first_name,
-            last_name: $scope.last_name,
-            status: "active"
-        };
+		if($scope.first_name && $scope.last_name){
+			var postData = {
+	            first_name: $scope.first_name,
+	            last_name: $scope.last_name,
+	            status: "active"
+	        };
 
-        userDataFactory.newUser(postData).then(function(response) {
-            if (response.status === 201) {
-                $route.reload();
-            }
-        }).catch(function(error) {
-            console.log(error);
-        });
+	        userDataFactory.newUser(postData).then(function(response) {
+	            if (response.status === 201) {
+	            	$scope.first_name = "";
+	            	$scope.last_name = "";
+	                $scope.user_created = true;
+	            }
+	        }).catch(function(error) {
+	            console.log(error);
+	        });
+		}
+	    
 	}
 }
